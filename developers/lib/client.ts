4088499a7610d9a67df72d8364ece508d118b213
Florian LEFEBVRE <contact@florian-lefebvre.dev>
2022-08-07 17:44:37 +0200

    migrate /developers to op-ent/developer-portal

--- developers/lib/client.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from "opent-js";
-
-const client = createClient({
-  baseUrl: process.env.NEXT_PUBLIC_API_URL || "http://localhost:3333/",
-  accessId: process.env.NEXT_PUBLIC_ACCESS_ID || "",
-});
-
-export default client;
