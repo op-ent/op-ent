c8c3f9ff3d04d8edd9d7d7042b7319f97e694c9e
Florian LEFEBVRE <contact@florian-lefebvre.dev>
2023-02-22 11:06:17 +0100

    feat(api): setup redis and docker-compose

--- apps/api/.eslintrc.js
+++ apps/api/.eslintrc.js
@@ -2,4 +2,5 @@
 module.exports = {
   root: true,
   extends: ['plugin:adonis/typescriptApp', 'shared'],
+  project: path.join(__dirname, 'tsconfig.json'),
 }
--- apps/api/env.ts
+++ apps/api/env.ts
@@ -27,4 +27,8 @@ export default Env.rules({
   PG_USER: Env.schema.string(),
   PG_PASSWORD: Env.schema.string.optional(),
   PG_DB_NAME: Env.schema.string(),
+  REDIS_CONNECTION: Env.schema.enum(['local'] as const),
+  REDIS_HOST: Env.schema.string({ format: 'host' }),
+  REDIS_PORT: Env.schema.number(),
+  REDIS_PASSWORD: Env.schema.string.optional(),
 })
