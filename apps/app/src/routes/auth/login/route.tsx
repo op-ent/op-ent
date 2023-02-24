cec629a4832db9eae6286d1869bdfbeecee1cfc7
Florian LEFEBVRE <contact@florian-lefebvre.dev>
2023-02-24 10:59:59 +0100

    chore(app): work on client side form validation

--- apps/app/src/routes/auth/login/route.tsx
+++ apps/app/src/routes/auth/login/route.tsx
@@ -1,15 +1,46 @@
 import { json, type ActionArgs, type LoaderArgs } from '@remix-run/node'
-import { Form, Link, useActionData, useTransition } from '@remix-run/react'
+import {
+  Form,
+  Link,
+  useActionData,
+  useSubmit,
+  useTransition,
+} from '@remix-run/react'
 import { AuthorizationError } from 'remix-auth'
 import { Button, Input } from 'shared-ui'
 import { login, withAuth } from '~/services/auth.server'
 import { Loader2 } from 'lucide-react'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useRef } from 'react'
+
+const schema = z.object({
+  email: z
+    .string()
+    .min(1, { message: 'Champ obligatoire' })
+    .email({ message: 'Email invalide' }),
+  password: z.string().min(6, { message: '6 caractères minimum' }),
+})
+
+type Schema = z.infer<typeof schema>
 
 export default function Login() {
   const transition = useTransition()
-  const errors = useActionData<typeof action>()
+  const actionErrors = useActionData<typeof action>()
 
   const loading = transition.state !== 'idle'
+
+  const formRef = useRef(null)
+  const submit = useSubmit()
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Schema>({
+    resolver: zodResolver(schema),
+    shouldUseNativeValidation: false,
+  })
 
   return (
     <div className="flex h-full flex-col items-center justify-center">
@@ -34,29 +65,31 @@ export default function Login() {
       </div>
       <Form
         method="post"
-        className="mx-auto grid w-full max-w-md gap-2 rounded-2xl border border-neutral-200 bg-white p-8 shadow-md dark:border-neutral-700 dark:bg-neutral-900"
+        ref={formRef}
+        onSubmit={handleSubmit((data) => submit(formRef.current))}
+        className="mx-auto grid w-full max-w-md grid-cols-1 gap-6 rounded-2xl border border-neutral-200 bg-white p-8 shadow-md dark:border-neutral-700 dark:bg-neutral-900"
       >
-        {errors && (
+        {actionErrors && (
           <pre className="bg-danger-500 text-white">
-            {JSON.stringify(errors, null, 2)}
+            {JSON.stringify(actionErrors, null, 2)}
           </pre>
         )}
         <Input
           label="Adresse email"
-          name="email"
           type="text"
           placeholder="exemple@gmail.com"
-          required
-          defaultValue="test@test.com"
+          // defaultValue="test@test.com"
+          // error={errors.email?.message}
+          {...register('email')}
         />
-        <Input
+        {/*<Input
           label="Mot de passe"
-          name="password"
           type="password"
           placeholder="••••••••"
-          required
-          defaultValue="123456"
-        />
+          // defaultValue="123456"
+          error={errors.password?.message}
+          {...register('password')}
+        /> */}
         <Button
           color="primary"
           variant="solid"
--- packages/shared-ui/lib/components/Input.tsx
+++ packages/shared-ui/lib/components/Input.tsx
@@ -8,7 +8,7 @@ const styles = tv({
       'focus-within:border-transparent focus-within:ring focus-within:ring-primary-300 dark:focus-within:ring-primary-600',
       'border border-neutral-200 dark:border-neutral-700',
       'bg-neutral-50 dark:bg-neutral-800',
-      'mb-4 rounded-md px-3 pt-2 pb-1 shadow-sm',
+      'rounded-md px-3 pt-2 pb-1 shadow-sm',
     ],
     label:
       'block text-xs font-medium select-none text-neutral-500 dark:text-neutral-400',
@@ -17,14 +17,24 @@ const styles = tv({
       'focus:outline-none focus:ring-0',
       'w-full border-0 bg-transparent p-0 text-sm leading-loose',
     ],
+    error: 'text-danger-600 mt-2 text-sm',
   },
 })
+// variants: {
+//   error: {
+//     wrapper: 'ring-danger-300 dark:ring-primary-600',
+//   },
+// },
+// defaultVariants: {
+//   error: false,
+// },
 
 type InputVariants = VariantProps<typeof styles>
 
 export interface InputProps extends InputVariants {
   as?: React.ElementType
   label: string
+  error?: string
 }
 
 type InputComponent = <C extends React.ElementType = 'input'>(
@@ -39,26 +49,52 @@ export const Input: InputComponent = forwardRef(
     const _id = useId()
     const {
       as = 'input',
-      label: _label,
+      label,
       id = _id,
       className,
+      error,
       ...inputProps
     } = props
-    const { wrapper, label, input } = styles()
+    const hasError = error !== undefined
+    // const { wrapper, label, input } = styles({ error: error !== undefined })
+    const {
+      wrapper: wrapperStyles,
+      label: labelStyles,
+      input: inputStyles,
+      error: errorStyles,
+    } = styles()
     const Component = as as React.ElementType
 
     return (
-      <div className={wrapper()}>
-        <label htmlFor={id} className={label()}>
-          {_label}
+      <div className={wrapperStyles()}>
+        <label htmlFor={id} className={labelStyles()}>
+          {label}
         </label>
         <Component
           ref={ref}
-          {...inputProps}
           id={id}
-          className={input({ className })}
+          className={inputStyles({ className })}
+          {...inputProps}
         />
       </div>
+      // <div>
+      //   <div className={wrapperStyles()}>
+      //     <label htmlFor={id} className={labelStyles()}>
+      //       {label}
+      //     </label>
+      //     <Component
+      //       ref={ref}
+      //       id={id}
+      //       className={inputStyles({ className })}
+      //       {...inputProps}
+      //     />
+      //   </div>
+      //   {hasError && (
+      //     <div role="alert" className={errorStyles()} id={`${id}-error`}>
+      //       {error}
+      //     </div>
+      //   )}
+      // </div>
     )
   }
 )
