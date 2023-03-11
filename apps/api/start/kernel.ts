d8c8770b49b35ce1a238a74664d8748dc217abb6
Florian LEFEBVRE <contact@florian-lefebvre.dev>
2023-03-11 18:46:15 +0100

    feat(api): add log request middleware

--- apps/api/start/kernel.ts
+++ apps/api/start/kernel.ts
@@ -21,6 +21,7 @@ import Server from '@ioc:Adonis/Core/Server'
 |
 */
 Server.middleware.register([
+  () => import('App/Middleware/Global/LogRequest'),
   () => import('@ioc:Adonis/Core/BodyParser'),
   () => import('App/Middleware/Global/SilentAuth'),
 ])
