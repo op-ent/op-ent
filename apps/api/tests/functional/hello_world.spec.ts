62c2e072a067a22f01812f06e6efa292a0530e16
Florian LEFEBVRE <contact@florian-lefebvre.dev>
2023-03-11 18:54:23 +0100

    test(api): add root page test

--- apps/api/tests/functional/hello_world.spec.ts
+++ apps/api/tests/functional/hello_world.spec.ts
@@ -1,8 +1,8 @@
 import { test } from '@japa/runner'
 
-test('display welcome page', async ({ client }) => {
+test('show docs link on root page', async ({ client }) => {
   const response = await client.get('/')
 
   response.assertStatus(200)
-  response.assertBodyContains({ hello: 'world' })
+  response.assertTextIncludes('docs.op-ent.fr')
 })
