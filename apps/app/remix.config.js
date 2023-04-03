4e33e35f08139e447e379c0aeed3aa46bebb81ed
NonozgYtb <nonozg.ytb@gmail.com>
2023-04-03 13:36:12 +0200

    fix: :memo: fix remix routes (routes not detected)

--- apps/app/remix.config.js
+++ apps/app/remix.config.js
@@ -1,3 +1,5 @@
+const { createRoutesFromFolders } = require('@remix-run/v1-route-convention')
+
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   ignoredRouteFiles: ['**/.*'],
@@ -5,7 +7,10 @@ module.exports = {
     process.env.NETLIFY || process.env.NETLIFY_LOCAL
       ? './server.js'
       : undefined,
-  serverBuildPath: '.netlify/functions-internal/server.js',
+  serverBuildPath:
+    process.env.NETLIFY || process.env.NETLIFY_LOCAL
+      ? '.netlify/functions-internal/server.js'
+      : undefined,
   serverDependenciesToBundle: [
     'shared-ui',
     'client',
@@ -20,9 +25,17 @@ module.exports = {
     'tailwind.config.js',
   ],
   future: {
+    // makes the warning go away in v1.15
     v2_routeConvention: true,
     v2_meta: true,
     unstable_tailwind: true,
+  },
+  routes(defineRoutes) {
+    // uses the v1 convention, works in v1.15+ and v2
+    return createRoutesFromFolders(defineRoutes, {
+      appDirectory: 'src',
+      routesDirectory: 'routes',
+    })
   },
   appDirectory: 'src',
   // assetsBuildDirectory: "public/build",
